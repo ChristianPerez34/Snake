42b9f0316e7868887d6a84775e73f09e9e2b17e7
Christian Perez Villanueva <lifelinelogics@Christians-MacBook-Pro-3.local>
2019-12-26 18:45:11 -0400

    Added snake and snake movement

--- snake.py
+++ snake.py
@@ -1,12 +1,42 @@
 import pygame
 
 game_over = False
-display = pygame.display.set_mode((400, 300))
+x_pos = 300
+y_pos = 300
+x_speed = 0
+y_speed = 0
+
+color = {
+    'lime': (0, 255, 0),
+    'white': (255, 255, 255)
+}
+
+display = pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock()
+
 pygame.display.update()
+pygame.display.set_caption('Lifeline Snake')
+
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
-        print(event)   #prints out all the actions that take place on the screen
-# pygame.quit()
-# quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                x_speed = 0
+                y_speed = 10
+            elif event.key == pygame.K_LEFT:
+                x_speed = -10
+                y_speed = 0
+            elif event.key == pygame.K_UP:
+                x_speed = 0
+                y_speed = -10
+            elif event.key == pygame.K_RIGHT:
+                x_speed = 10
+                y_speed = 0
+    x_pos += x_speed
+    y_pos += y_speed
+    display.fill(color['white'])
+    pygame.draw.rect(display, color['lime'], [x_pos, y_pos, 10, 10])
+    pygame.display.update()
+    clock.tick(25)
